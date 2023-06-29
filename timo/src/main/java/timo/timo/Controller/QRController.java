5b6d075b371e5d55aa11aac32c19b3482cea08fc
jihoon <wlgnsdl1016@naver.com>
2023-06-29 10:48:05 +0900

    qr

--- timo/src/main/java/timo/timo/Controller/QRController.java
+++ timo/src/main/java/timo/timo/Controller/QRController.java
@@ -1,10 +1,21 @@
 package timo.timo.Controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.reactive.function.client.WebClient;
 import timo.timo.Service.QRService;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
 
 @Controller
 @RequiredArgsConstructor
@@ -23,6 +34,40 @@ public class QRController {
             return "redirect:https://quickchart.io/qr?text=https://www.google.com/";
         }
 
+    }
+
+    @GetMapping("/qrtest")
+    @ResponseBody
+    public void qrtest() throws IOException {
+
+        WebClient client3 = WebClient.builder()
+                .baseUrl("https://quickchart.io/qr")
+                //.defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .build();
+
+
+        byte[] response = client3.get()
+                .uri(uriBuilder -> uriBuilder
+                        .queryParam("text", "https://www.naver.com/")
+                        .build())
+                .accept(MediaType.IMAGE_PNG)
+                .retrieve()
+                .bodyToMono(byte[].class)
+                .block();
+
+        ByteArrayInputStream bis = new ByteArrayInputStream(response);
+        BufferedImage bImage2 = ImageIO.read(bis);
+        ImageIO.write(bImage2, "png", new File("output.png") );
+        System.out.println("image created");
+
+//        ImageIcon imageIcon = new ImageIcon(response);
+//        return imageIcon.getImage();
+//
+//        //System.out.println("response = " + response);
+//
+//        //System.out.println("response = " + response);
 
     }
+
+
 }
